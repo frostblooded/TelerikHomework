d51a30d630c45d4c7e1327f36f3969707cd30ef6
Nikolay <frostblooded@yahoo.com>
2014-09-28 09:32:42 +0300

    Made the StringBuilder extension

---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/SubstringExtensionMain/SubstringExtensionMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace SubstringExtensionMain
+{
+    class SubstringExtensionMain
+    {
+        static void Main()
+        {
+            string someString = "SomeRandomString";
+            Console.WriteLine(someString.Substring(5, 7));
+            StringBuilder stringBuilder = new StringBuilder("SomeRandomString");
+            Console.WriteLine(stringBuilder.Substring(5, 7));
+        }
+    }
+}
