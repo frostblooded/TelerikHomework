2575e762037f948338c85330902eae5788b4cb77
Nikolay <frostblooded@yahoo.com>
2014-09-28 12:59:39 +0300

    Did the task about the IEnumerable<T> extension methods

---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/IEnumerableExtensionsMain/IEnumerableExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerableExtensionsMain
+{
+    public static class IEnumerableExtensions
+    {
+        public static T Sum<T>(this IEnumerable<T> collection)
+        where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("The collection is empty or isn't initialized!");
+            }
+            else
+            {
+                T sum = default(T);
+
+                foreach (var item in collection)
+                {
+                    sum += (dynamic)item;
+                }
+
+                return sum;
+            }
+        }
+
+        public static T Product<T>(this IEnumerable<T> collection)
+        where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("The collection is empty or isn't initialized!");
+            }
+            else
+            {
+                T product = (dynamic)1;
+
+                foreach (var item in collection)
+                {
+                    product *= (dynamic)item;
+                }
+
+                return product;
+            }
+        }
+
+        public static T Min<T>(this IEnumerable<T> collection)
+        where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("The collection is empty or isn't initialized!");
+            }
+            else
+            {
+                T min = collection.First();
+
+                foreach (var item in collection)
+                {
+                    if (min.CompareTo(item) > 0)
+                    {
+                        min = item;
+                    }
+                }
+
+                return min;
+            }
+        }
+
+        public static T Max<T>(this IEnumerable<T> collection)
+        where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("The collection is empty or isn't initialized!");
+            }
+            else
+            {
+                T max = default(T);
+
+                foreach (var item in collection)
+                {
+                    if (max.CompareTo(item) < 0)
+                    {
+                        max = item;
+                    }
+                }
+
+                return max;
+            }
+        }
+
+        public static T Average<T>(this IEnumerable<T> collection)
+        where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("The collection is empty or isn't initialized!");
+            }
+            else
+            {
+                T result = default(T);
+                int counter = 0;
+
+                foreach (var item in collection)
+                {
+                    result += (dynamic)item;
+                    counter++;
+                }
+
+                return result / (dynamic)counter;
+            }
+        }
+    }
+}
---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/IEnumerableExtensionsMain/IEnumerableExtensionsMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableExtensionsMain
+{
+    class IEnumerableExtensionsMain
+    {
+        static void Main()
+        {
+            List<int> someList = new List<int>();
+            someList.Add(4);
+            someList.Add(3);
+            someList.Add(5);
+            someList.Add(6);
+            someList.Add(1);
+            someList.Add(2);
+            Console.WriteLine(someList.Sum());
+            Console.WriteLine(someList.Product());
+            Console.WriteLine(someList.Min());
+            Console.WriteLine(someList.Max());
+            Console.WriteLine(someList.Average());
+        }
+    }
+}
