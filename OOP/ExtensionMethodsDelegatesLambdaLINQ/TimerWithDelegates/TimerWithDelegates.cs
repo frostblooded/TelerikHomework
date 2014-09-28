bf9a7a938f7e94d8ae7dd19240c073d13e3ca63a
Nikolay <frostblooded@yahoo.com>
2014-09-28 20:18:40 +0300

    Created a class Timer

---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/TimerWithDelegates/TimerWithDelegates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TimerWithDelegates
+{
+    class TimerWithDelegates
+    {
+        static void Main()
+        {
+            Timer timer = new Timer();
+            timer.Start(3000, Console.Write, "String.");
+            timer.Start(3000, Console.Write, "Sub");
+            timer.Start(3000, Console.WriteLine, "String");
+        }
+    }
+}
