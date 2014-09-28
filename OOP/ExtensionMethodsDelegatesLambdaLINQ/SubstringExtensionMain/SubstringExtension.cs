49c87c1ed9e4fa9d879e9a25aac653026c34bbc8
Nikolay <frostblooded@yahoo.com>
2014-09-28 12:54:20 +0300

    Revert "Revert "Made the StringBuilder extension""
    This reverts commit ece632a58350b9f9996b6bf8f93ec47fa7b9c022.

---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/SubstringExtensionMain/SubstringExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace SubstringExtensionMain
+{
+    public static class SubstringExtension
+    {
+        public static StringBuilder Substring(this StringBuilder original, int index, int length)
+        {
+            if (index > original.Length)
+            {
+                throw new ArgumentOutOfRangeException("The index is bigger than the length of the StringBuilder object.");
+            }
+            else if (index + length > original.Length)
+            {
+                throw new ArgumentOutOfRangeException("The requested length is too big.");
+            }
+            else
+            {
+                StringBuilder result = new StringBuilder();
+
+                for (int i = index; i < index + length; i++)
+                {
+                    result.Append(original[i]);
+                }
+
+                return result;
+            }
+        }
+    }
+}
