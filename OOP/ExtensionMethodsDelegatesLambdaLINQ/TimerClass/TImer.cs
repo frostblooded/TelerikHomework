2c7e00d4f6f1c8db33babb646c12219ea649cf81
Nikolay <frostblooded@yahoo.com>
2014-09-29 10:31:53 +0300

    Made the timer with events (poorly)

---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/TimerClass/TImer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerClass
+{
+    using System.Threading;
+
+    public class Timer
+    {
+        // The event that will be raised when time elapses
+        public event EventHandler TimeElapsed;
+
+        public int TicksCount { get; private set; }
+
+        public int Interval { get; private set; }
+
+        public Timer(int ticksCount, int interval)
+        {
+            this.TicksCount = ticksCount;
+            this.Interval = interval;
+        }
+
+        /// <summary>
+        /// The method which raises the event.
+        /// </summary>
+        /// <param name="ticks"></param>
+        protected void OnTimeElapsed(int ticks)
+        {
+            if (TimeElapsed != null)
+            {
+                EventArgs e = new EventArgs();
+                TimeElapsed(this, e);
+            }
+        }
+
+        public void Run()
+        {
+            int ticks = this.TicksCount;
+            while (ticks > 0)
+            {
+                Thread.Sleep(this.Interval);
+                ticks--;
+                OnTimeElapsed(ticks);
+            }
+        }
+    }
+}
---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/TimerClass/TimerClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerClass
+{
+    class TimerClass
+    {
+        static void Main()
+        {
+            Timer timer = new Timer(4, 1500);
+            timer.TimeElapsed += timer_TimeElapsed;
+            timer.Run();
+        }
+
+        static void timer_TimeElapsed(object sender, EventArgs e)
+        {
+            Console.WriteLine("The timer has elapsed");
+        }
+    }
+}
