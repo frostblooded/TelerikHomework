d1716b1661f3d51be416d9a382aacca6ea8a3d73
Nikolay <frostblooded@yahoo.com>
2014-09-29 10:45:44 +0300

    Made getting students from second group and sorting them by first name

---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/Students/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Students
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, int groupNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.GroupNumber = groupNumber;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string FN { get; private set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+        public List<int> Marks { get; set; }
+        public int GroupNumber { get; set; }
+
+        public override string ToString()
+        {
+            return this.FirstName + " " + this.LastName + " " + this.GroupNumber;
+        }
+    }
+}
---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/Students/Students.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students
+{
+    class Students
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student("Nikolay", "Danailov", 1),
+                new Student("Anonio", "Mindov", 1),
+                new Student("Petar", "Petrov", 2),
+                new Student("Histo", "Ezekiev", 2)
+            };
+
+            var studentsFromSecondGroup =
+                from st in students
+                where st.GroupNumber == 2
+                orderby st.FirstName
+                select st;
+
+            foreach (var student in studentsFromSecondGroup)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
