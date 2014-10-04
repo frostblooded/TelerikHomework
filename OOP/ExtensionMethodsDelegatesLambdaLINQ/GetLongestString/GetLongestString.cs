ab565059f48dc0e9ce8449dc784fbc7302115a56
Nikolay <frostblooded@yahoo.com>
2014-10-04 10:41:20 +0300

    Made getting longest string with Linq

---
+++ OOP/ExtensionMethodsDelegatesLambdaLINQ/GetLongestString/GetLongestString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace GetLongestString
+{
+    class GetLongestString
+    {
+        static void Main()
+        {
+            string[] strings = new string[] { "The", "man", "in", "black", "fled", "across", "the", "dessert", "and", "the", "gunslinger", "followed" };
+
+            string longestString = strings.OrderByDescending(st => st.Length).First();
+
+            Console.WriteLine(longestString);
+        }
+    }
+}
