c679e1b452f4b1c477a4024b74bd36af3db62c81
Nikolay <frostblooded@yahoo.com>
2014-10-24 10:21:42 +0300

    Completed the InvalidException task and the OOP Principles Part 2 Homework with it

---
+++ OOP/ObjectOrientedProgrammingPrinciplesPart2/ExceptionSystem/ExceptionSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExceptionSystem
+{
+    class ExceptionSystem
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter 5 numbers");
+
+            for (int i = 0; i < 5; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 1 || number > 100)
+                {
+                    throw new InvalidRangeException<int>("The number wasn't in the valid range!", new Exception(), 1, 100);
+                }
+            }
+
+            Console.WriteLine("Enter a date!");
+
+            DateTime dateTime = DateTime.Parse(Console.ReadLine());
+
+            if (dateTime < new DateTime(1980, 1, 1) || dateTime > new DateTime(2013, 12, 31))
+            {
+                throw new InvalidRangeException<DateTime>("The date wasn't in the valid format!", new Exception(), new DateTime(1980, 1, 1), new DateTime(2013, 12, 31));
+            }
+        }
+    }
+}
---
+++ OOP/ObjectOrientedProgrammingPrinciplesPart2/ExceptionSystem/InvalidRangeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExceptionSystem
+{
+    public class InvalidRangeException<T> : ApplicationException
+    {
+        public InvalidRangeException()
+        {
+        }
+
+        public InvalidRangeException(string msg, Exception innerException, T minRange, T maxRange)
+            :base(msg, innerException)
+        {
+            MinRange = minRange;
+            MaxRange = maxRange;
+        }
+
+        public T MinRange { get; set; }
+        public T MaxRange { get; set; }
+    }
+}
